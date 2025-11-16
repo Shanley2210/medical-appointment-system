e544ea2092aa1f70734ac5f2672b9e3ed17967d3
Shanley <nguyenhieushanley2210@gmail.com>
2025-11-16 16:24:49 +0700

    setup frontend

---
+++ frontend/src/shared/configs/i18n.ts
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import vi from '@/shared/locales/vi.json';
+import en from '@/shared/locales/en.json';
+
+i18n.use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: { translation: en },
+            vi: { translation: vi }
+        },
+        fallbackLng: 'vi',
+        interpolation: {
+            escapeValue: false
+        }
+    });
+
+export default i18n;
---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@internal': path.resolve(__dirname, './src/internal'),
+            '@patient': path.resolve(__dirname, './src/patient'),
+            '@shared': path.resolve(__dirname, './src/shared'),
+            '@router': path.resolve(__dirname, './src/router')
+        }
+    }
+});
